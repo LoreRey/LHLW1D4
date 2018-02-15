111fe95feff2b85b320fa501e1d7930f7520e5cb
LoreRey <lorena.n.reyes@gmail.com>
2018-02-15 16:39:56 +0000

    found Waldo by index using forEach loop

---
+++ findWaldo2.js
@@ -0,0 +1,13 @@
+function findWaldo(arr, found) {
+  arr.forEach(function(item, index) {
+    if (item === "Waldo") {
+      found(index);   // execute callback
+    }
+  } );
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index " + index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
