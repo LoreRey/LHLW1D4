d76a588be4ab94ee7b692f50e916454d1002e6cc
LoreRey <lorena.n.reyes@gmail.com>
2018-02-15 18:05:00 +0000

    callbacks anonymous functions

---
+++ arrayMapping.js
@@ -0,0 +1,16 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function(numbers) {
+
+  return Math.sqrt(Math.pow(numbers.x, 2) + Math.pow(numbers.y, 2))
+
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
+
+
